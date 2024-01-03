a23d1b16baf0adf8491630a386c4c125de883a1a
John-Dembaremba <johndembaremba97@gmail.com>
2024-01-03 16:13:56 +0200

    testing my_app.

---
+++ my_app/main.py
@@ -0,0 +1,12 @@
+import time
+
+
+def main():
+    my_name = (
+        f"=====>>>>> Yes it`s deployed. \n {time.sleep(2)} ====>>> Deployed finished. "
+    )
+    return my_name
+
+
+if __name__ == "__main__":
+    print(main())
